1a31533714405ffadd155fa90fd1841c2fed294f
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2019-02-12 10:48:37 -0500

    Get list of owned tokens at init

--- components/BadgeDroper.py
+++ components/BadgeDroper.py
@@ -6,7 +6,24 @@ class BadgeDropper:
     def __init__(self, Web3Service: Web3Service):
         self.w3Service = Web3Service
         self.tokenContract = self.w3Service.eth.contract(address=self.w3Service.w3.toChecksumAddress('0xabe71e6a260c2eea3c30864dc50639100aa315f6'),abi=abi)
+        self.getOwnedTokens()
         print("Started badge dropper")
+    
+    def getOwnedTokens(self):
+        self.tokenCount = self.tokenContract.functions.balanceOf(self.w3Service.accountPBK).call() # get amount of owned tokens
+        self.ownedTokens = [] # list used to keep track of the owned tokens
+        for i in range(0, self.tokenCount):
+            # iterate through the ownedTokens array of the contract
+            tokenID = self.tokenContract.functions.tokenOfOwnerByIndex(self.w3Service.accountPBK,i).call()
+            self.ownedTokens.append(tokenID) # add each tokenID to the list
+
+    # Get the next tokenID in the list to send and delete from list
+    def getNexTokenID(self):
+        if (len(self.ownedTokens) == 0):
+            print("Ran out of tokens")
+            return 
+        return self.ownedTokens.pop()
+
     def send721Token(self, token_ID, dest_address):
         dest_address = self.w3Service.w3.toChecksumAddress(dest_address)
         safe_transfer = self.tokenContract.functions.safeTransferFrom(self.w3Service.accountPBK,dest_address,token_ID) # build the function to send tx
--- services/Web3Service.py
+++ services/Web3Service.py
@@ -9,7 +9,7 @@ class Web3Service:
         self.w3 = Web3(Web3.HTTPProvider(http_endpoint))
         self.eth = Eth(self.w3)
         self.accountPK = os.environ.get('PK')
-        self.accountPBK = self.w3.toChecksumAddress("0xdcA85d54CBE2C03B8f7E60F2620D5765f334C423")
+        self.accountPBK = self.w3.toChecksumAddress("0xDABaFC6E01495a90aB745B25E1d754B38560aAe0")
         print("Started web3 service")
 
     def sign_tx(self,contractFunc,options=None): # This will generate a signed tx with the private key of the service
