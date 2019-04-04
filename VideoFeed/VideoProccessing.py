531fbb254b2d9c10a1da4a955bf578aa21d4a3f7
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2019-04-04 13:01:38 -0500

    Display imgae of the token as overlay on video feed

--- VideoFeed/VideoProccessing.py
+++ VideoFeed/VideoProccessing.py
@@ -6,7 +6,6 @@ from web3.auto.infura import w3
 from components.BadgeDroper import BadgeDropper
 from threading import Timer
 
-#
 class VideoProcessor:
     should_capture = True
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -15,10 +14,12 @@ class VideoProcessor:
         self.badge_dropper = badge_dropper
         self.sent = False
         self.processed_addresses = []
+        self.tokenImage = cv2.imread(self.badge_dropper.badge_image_path,cv2.IMREAD_UNCHANGED)
+        self.tokenImage = cv2.resize(self.tokenImage, (200,200)) 
         print("Started VideoProcessor")
     
+    # Calback function used to reset the should_capture flag and decode the frame again
     def resetCapture(self):
-        print("Callback fired")
         self.should_capture = True
         
     def lookForQR(self):
@@ -26,10 +27,25 @@ class VideoProcessor:
             ret,frame = self.source.read()
             barcodes = pyzbar.decode(frame)
             if(len(barcodes) > 1):
+                print(barcodes[0].type)
                 cv2.putText(frame,"Please only show ONE QR code",(100,100), self.font, 1, (255,0,0), 3, cv2.LINE_AA)
                 continue
             if(len(barcodes) == 1):
-                frame = self.parseQR(barcodes,frame)
+                if (barcodes[0].type == 'QRCODE'):
+                    frame = self.parseQR(barcodes,frame)
+            #frame[50:50+self.tokenImage.shape[0],50:50+self.tokenImage.shape[1]] = self.tokenImage
+            y_offset = 100
+            x_offset = 100
+
+            y1, y2 = y_offset, y_offset + self.tokenImage.shape[0]
+            x1, x2 = x_offset, x_offset + self.tokenImage.shape[1]
+
+            alpha_s = self.tokenImage[:, :, 2] / 255.0
+            alpha_l = 1.0 - alpha_s
+
+            for c in range(0, 3):
+                frame[y1:y2, x1:x2, c] = (alpha_s * self.tokenImage[:, :, c] +
+                                        alpha_l * frame[y1:y2, x1:x2, c])
             cv2.imshow('frame',frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -48,11 +64,40 @@ class VideoProcessor:
                 cv2.putText(frame,"You already claimed this badge",(x,y), self.font, 1, (255,0,0), 3, cv2.LINE_AA)
             else:
                 self.processed_addresses.append(eth_address)
-                token_id = self.badge_dropper.getNexTokenID()
+                token_id = self.badge_dropper.getNextTokenID()
                 self.badge_dropper.send721Token(token_id,eth_address)          
                 cv2.putText(frame,"Badge was sent succesfully",(x,y), self.font, 1, (255,0,0), 3, cv2.LINE_AA)
                 self.should_capture = False
                 Timer(2.5,self.resetCapture).start()
         return frame
 
+    def overlay_image_alpha(self,img, img_overlay, pos, alpha_mask):
+        """Overlay img_overlay on top of img at the position specified by
+        pos and blend using alpha_mask.
 
+        Alpha mask must contain values within the range [0, 1] and be the
+        same size as img_overlay.
+        """
+
+        x, y = pos
+
+        # Image ranges
+        y1, y2 = max(0, y), min(img.shape[0], y + img_overlay.shape[0])
+        x1, x2 = max(0, x), min(img.shape[1], x + img_overlay.shape[1])
+
+        # Overlay ranges
+        y1o, y2o = max(0, -y), min(img_overlay.shape[0], img.shape[0] - y)
+        x1o, x2o = max(0, -x), min(img_overlay.shape[1], img.shape[1] - x)
+
+        # Exit if nothing to do
+        if y1 >= y2 or x1 >= x2 or y1o >= y2o or x1o >= x2o:
+            return
+
+        channels = img.shape[2]
+
+        alpha = alpha_mask[y1o:y2o, x1o:x2o]
+        alpha_inv = 1.0 - alpha
+
+        for c in range(channels):
+            img[y1:y2, x1:x2, c] = (alpha * img_overlay[y1o:y2o, x1o:x2o, c] +
+                                    alpha_inv * img[y1:y2, x1:x2, c])
--- components/BadgeDroper.py
+++ components/BadgeDroper.py
@@ -1,5 +1,8 @@
 from services.Web3Service import Web3Service
 from services.TokenABI import abi
+import requests
+import shutil
+import os
 
 class BadgeDropper:
 
@@ -7,10 +10,15 @@ class BadgeDropper:
         self.w3Service = Web3Service
         self.tokenContract = self.w3Service.eth.contract(address=self.w3Service.w3.toChecksumAddress('0xabe71e6a260c2eea3c30864dc50639100aa315f6'),abi=abi)
         self.getOwnedTokens()
+        self.badge_image_path = 'burnerlogo.png' 
+        self.getTokenMetadata(self.ownedTokens[0])
         print("Started badge dropper")
     
     def getOwnedTokens(self):
         self.tokenCount = self.tokenContract.functions.balanceOf(self.w3Service.accountPBK).call() # get amount of owned tokens
+        if(self.tokenCount == 0):
+            print("The badge dropper ran out of tokens :(")
+            exit() 
         self.ownedTokens = [] # list used to keep track of the owned tokens
         for i in range(0, self.tokenCount):
             # iterate through the ownedTokens array of the contract
@@ -18,7 +26,7 @@ class BadgeDropper:
             self.ownedTokens.append(tokenID) # add each tokenID to the list
 
     # Get the next tokenID in the list to send and delete from list
-    def getNexTokenID(self):
+    def getNextTokenID(self):
         if (len(self.ownedTokens) == 0):
             print("Ran out of tokens")
             return 
@@ -31,3 +39,17 @@ class BadgeDropper:
         txReceipt = self.w3Service.eth.sendRawTransaction(signed_tx.rawTransaction)
         txHash = self.w3Service.w3.toHex(txReceipt)
         print(txHash)
+    
+    def getTokenMetadata(self, tokenID):
+        metadata_uri = self.tokenContract.functions.tokenURI(tokenID).call()
+        req = requests.get(metadata_uri)
+        data = req.json()
+        print(data)
+        self.badge_name = data['name']
+        self.downloadImage(data['image'])
+    
+    def downloadImage(self,url):
+        response = requests.get(url, stream=True)
+        with open(self.badge_image_path , 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        
